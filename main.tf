dd5ddb4e8015ebffc9919cbdc37035ebfb037ef0
rollingmountains <balakrishnapaudel@gmail.com>
2023-07-28 15:05:45 +0545

    chore-alignment

--- main.tf
+++ main.tf
@@ -15,7 +15,6 @@ resource "aws_vpc" "prod-vpc" {
 }
 
 # Create a subnet for VPC
-
 resource "aws_subnet" "prod-subnet" {
   vpc_id            = aws_vpc.prod-vpc.id
   cidr_block        = "10.0.1.0/24"
