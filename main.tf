7af214355d4e7864dc2ecf3a649756a7cb11517d
rollingmountains <balakrishnapaudel@gmail.com>
2023-07-28 15:03:18 +0545

    feature/create-vpc-subnet

--- main.tf
+++ main.tf
@@ -3,3 +3,25 @@ provider "aws" {
   access_key = "<key>"
   secret_key = "<secret key>"
 }
+
+
+# Create a VPC
+resource "aws_vpc" "prod-vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "prod-vpc"
+  }
+}
+
+# Create a subnet for VPC
+
+resource "aws_subnet" "prod-subnet" {
+  vpc_id            = aws_vpc.prod-vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "prod-subnet"
+  }
+}
